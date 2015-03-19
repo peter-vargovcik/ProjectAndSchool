2778c11df5d9915d5a9e9b46906ddd0f8a3a96d1
Peter Vargovcik <peter.vargovcik@gmail.com>
2015-03-19 00:50:40 +0000

    search alogrithn v1 programmed

---
+++ Project4/src/vargovcik/peter/interfaces/SearchInterface.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vargovcik.peter.interfaces;
+
+/**
+ *
+ * @author Peter Vargovcik
+ */
+public interface SearchInterface {
+    void searchPaused();
+    
+}
